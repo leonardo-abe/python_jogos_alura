813da126b084412809d08e8b3aa42f16313d1947
Leonardo Tadanory Abe <leonardo.abe@nagumo.com.br>
2022-02-20 15:24:40 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,25 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+print(float(3//2))
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+print("Você digitou " , chute_str)
+chute = int(chute_str)
+
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+print(type(acertou))
+if(acertou):
+    print("Parabéns! Você acertou!")
+else:
+    if(maior):
+        print("O seu chute foi maior do que o número secreto!")
+    elif(menor):
+        print("O seu chute foi menor do que o número secreto!")
+
+print("Fim do jogo")
